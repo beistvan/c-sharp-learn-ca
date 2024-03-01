daea52ac10494faf66c4387de93c85ece90ea24b
Beregszászi István <beistvan@gmail.com>
2024-03-01 22:52:59 +0100

    Comments In C#

---
+++ comments-in-c-sharp.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace GettingInput
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Comment
+      Console.WriteLine("How old are you?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"You are {input} years old!");
+    }
+  }
+}
+
+/* This variable will be used to count the number of times anyone tweets the word persnickety */
+// int persnicketyCount = 0;
+
+/* Calculates tomorrow's rain likelihood as a number between 0 and 100 */
+// ComplicatedRainCalculationForTomorrow();
+
+// string usefulValue = OldSloppyCode();
+// string usefulValue = NewCleanCode();
+
