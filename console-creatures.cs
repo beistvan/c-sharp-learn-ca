8db497a459e5050b5e225e8a0609f70e683e050d
Beregszászi István <beistvan@gmail.com>
2024-03-01 22:57:07 +0100

    Console Creatures - C#

---
+++ console-creatures.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Some creature created
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)"); 
+      Console.WriteLine("| o |"); 
+      Console.WriteLine("|   |"); 
+      Console.WriteLine("'~~~`"); 
+      Console.WriteLine("Spiro the Specter");
+      Console.WriteLine("c_     ___");
+　    Console.WriteLine("   \\\\__|__|__");
+　　  Console.WriteLine("     \\( o_o)");
+      Console.WriteLine("　　　 > ~  >");
+      Console.WriteLine("　　　/ 　 / \\\\");
+      Console.WriteLine("　　 /　　/　 \\\\");
+      Console.WriteLine("　　 \\　 )　　c/");
+      Console.WriteLine("　　/　 /");
+      Console.WriteLine("　 /　/|");
+      Console.WriteLine("　(　( \\");
+      Console.WriteLine("　|　|  \\");
+      Console.WriteLine("　| / \\  )");
+      Console.WriteLine("　| |　) |");
+      Console.WriteLine(" /  )(_/");
+      Console.WriteLine("(_ /");
+      Console.WriteLine("Dancing Man");
+    }
+  }
+}
