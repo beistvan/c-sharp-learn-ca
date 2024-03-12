7d054ad66302bf06c611a6911309c2de465e542b
Beregszászi István <beistvan@gmail.com>
2024-03-12 14:01:57 +0100

    Handling Errors

---
+++ handling-errors.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace BugSquasher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int number = 384983222;
+
+      string dinosaur = "Barney";
+
+      double locked = 293.0;
+
+      bool is_yes = true;
+
+      string band = "The Low Anthem";
+
+    }
+  }
+}
+
+/*
+randomData = "asdf jskdf";
+Console.WriteLine(randomData);
+Error:
+The name 'randomData' does not exist in the current context [CS0103:] 
+
+int score = 45.39;
+Error:
+Cannot implicitly convert type 'double' to 'int'. An explicit conversion exists (are you missing a cast?)
+
+string iAmAVariable;
+string i'mnot; // error
+*/
