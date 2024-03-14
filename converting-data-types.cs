6d6a945047ff73547127a68ee60f41263e825574
Beregszászi István <beistvan@gmail.com>
2024-03-14 10:16:03 +0100

    Working With Numbers

---
+++ converting-data-types.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace FavoriteNumber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Ask user for fave number
+      Console.Write("Enter your favorite number!: ");
+      
+      // Turn that answer into an int
+      int faveNumber = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+/* 
+double myDouble = 3.2;
+
+// Round myDouble to the nearest whole number
+int myInt = myDouble;
+*/
+
+/*
+int myInt = 3;
+// Turn it into a double
+double myDouble = myInt;
+*/
+
+/*
+double myDouble = 3.2;
+
+// Round myDouble to the nearest whole number
+int myInt = (int)myDoubledouble myDouble = 3.2;
+
+// Round myDouble to the nearest whole number
+// int myInt = (int)myDouble;
+*/
